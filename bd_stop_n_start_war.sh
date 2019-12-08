1714cde9ccf03aa1d790e5f85905260d611af026
shipk <shipkovk@gmail.com>
2019-12-08 21:15:35 +0300

    .

--- bd_stop_n_start_war.sh
+++ bd_stop_n_start_war.sh
@@ -14,6 +14,6 @@ sleep 5
 cd /home/kshipkov/github/BookingDemo
 echo `pwd`
 #/usr/bin/nohup /usr/bin/java -jar /home/kshipkov/github/BookingDemo/target/BookingDemo-0.0.1-SNAPSHOT.war &
-/usr/bin/java -jar /home/kshipkov/github/BookingDemo/target/BookingDemo-0.0.1-SNAPSHOT.war
+nohup /usr/bin/java -jar /home/kshipkov/github/BookingDemo/target/BookingDemo-0.0.1-SNAPSHOT.war &
 sleep 10
 echo Started web server, port 8082
